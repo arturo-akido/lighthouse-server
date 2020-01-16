e210d20ca225a0c11cc2a2fa275e2044ac4a6fcb
Arturo Coronel <arturo@akidolabs.com>
2020-01-16 11:10:27 +0900

    change deploy script

--- scripts/deploy_qa.sh
+++ scripts/deploy_qa.sh
@@ -1,13 +1,17 @@
 #!/bin/bash -ex
 source $(git rev-parse --show-toplevel)/scripts/lib/akido_lib.sh
 
-DESTROY_FIRST=false
+# Uncomment this to destroy the app before deploying the new version
+# DESTROY_FIRST=true
+
 DEPLOY_BRANCH=${1:-master} # default to master branch but allow any branch.
 # This will be the name and URL of your app. Should be short, memorable, and
 # should use dashes ("-") instead of underscores ("_").
 APP_NAME="lighthouse-ci"
 DEPLOY_ENVIRONMENT=qa
-DOKKU_USES_REDIS=false
+
+DOKKU_USES_POSTGRES=false
+DOKKU_USES_REDIS=true
 
 DOKKU_HOST="qa-dokku.dev.corp.akidolabs.com"
 DOKKU_CMD="ssh dokku@${DOKKU_HOST}"
@@ -16,5 +20,6 @@ git_merge_origin
 
 dokku_destroy_first
 dokku_create_app || fail
+dokku_git_push || fail
 
 echo "Deploy completed"
