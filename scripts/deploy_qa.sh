9542665f2a7b98622bf7b944fe821872e18f00d6
Arturo Coronel <arturo@akidolabs.com>
2021-08-30 15:08:24 -0700

    Map dokku ports

--- scripts/deploy_qa.sh
+++ scripts/deploy_qa.sh
@@ -22,8 +22,9 @@ dokku_destroy_first
 dokku_create_app_and_database || fail
 
 $DOKKU_CMD -- config:set --no-restart ${APP_NAME} \
+  DOKKU_PROXY_PORT_MAP="http:80:5000 https:443:5000" \
   || FAIL
-  
+
 dokku_git_push || fail
 
 echo "Deploy completed"
